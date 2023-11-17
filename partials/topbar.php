cf0f6483193d17d13d0cbd5efb7dc187db1ac6c2
tupe2207 <tophercastano@gmail.com>
2023-11-17 14:32:16 +0800

    icons topbar

--- partials/topbar.php
+++ partials/topbar.php
@@ -1,7 +1,21 @@
 <nav class="navbar navbar-expand-lg" style="background-color: var(--bs-content-bg); border-bottom: var(--bs-border-width) solid var(--bs-content-border-color); width: 100%;">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-    <img src="./img/prime.png" alt="Logo"height="24" class="d-inline-block align-text-top">
+  <img src="./img/prime.png" alt="Logo"height="24" class="d-inline-block align-text-top">
+<span><ion-icon name="newspaper-outline"></ion-icon></span>
+<span><ion-icon name="newspaper-outline"></ion-icon></span>
+<span><ion-icon name="file-tray-full-outline"></ion-icon></span>
+<span><ion-icon name="document-text-outline"></ion-icon></span>
+<span><ion-icon name="download-outline"></ion-icon></span>
+<span><ion-icon name="cloud-download-outline"></ion-icon></span>
+<span><ion-icon name="archive-outline"></ion-icon></span>
+<span><ion-icon name="book-outline"></ion-icon></span>
+<span><ion-icon name="bulb-outline"></ion-icon></span>
+<span><ion-icon name="clipboard-outline"></ion-icon></span>
+<span><ion-icon name="desktop-outline"></ion-icon></span>
+<span><ion-icon name="file-tray-stacked-outline"></ion-icon></span>
+<span><ion-icon name="grid-outline"></ion-icon></span>
+<span><ion-icon name="log-out-outline"></ion-icon></span>
 
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-collapse-1" aria-controls="navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
