76d136267315c3a3c3bff38703ac9144d04d9cb8
Roy <95271521+Ryotard@users.noreply.github.com>
2023-12-09 10:42:28 +0800

    sidebar fix

--- partials/footer.php
+++ partials/footer.php
@@ -39,12 +39,23 @@
 
             
 
-    
+        // JavaScript to handle the active state
+        document.querySelectorAll('.icon').forEach(function(icon) {
+      icon.addEventListener('click', function() {
+        // Remove "active" class from all icons
+        document.querySelectorAll('.icon').forEach(function(icon) {
+          icon.classList.remove('active');
+        });
+
+        // Add "active" class to the clicked icon
+        icon.classList.add('active');
+      });
+    });
 
     </script>
     <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script> -->
 
 
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
