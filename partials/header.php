5c5528d188c2b51f9dc1fb56bf2f9fc424daeed9
Roy <95271521+Ryotard@users.noreply.github.com>
2023-12-14 12:01:30 +0800

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.css">
     <script src="./node_modules//jquery/dist/jquery.min.js" defer></script>
     <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" defer></script>
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js" defer></script>
+
     <title>Document</title>
 </head>
 <body>
