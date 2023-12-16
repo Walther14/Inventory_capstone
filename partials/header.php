d0fe6ee4416712acfb9851eba3e9924f08197bf3
Roy <95271521+Ryotard@users.noreply.github.com>
2023-12-16 15:55:31 +0800

    backup and restore

--- partials/header.php
+++ partials/header.php
@@ -6,7 +6,9 @@
     <link rel="stylesheet" href="./node_modules//halfmoon/css/halfmoon.min.css">
     <link rel="stylesheet" href="./css/custom.css">
     <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.css">
+    <link rel="stylesheet" href="./node_modules/sweetalert2/dist/sweetalert2.min.css">
     <script src="./node_modules//jquery/dist/jquery.min.js" defer></script>
+    <script src="./node_modules/sweetalert2/dist/sweetalert2.all.min.js" defer></script>
     <!-- <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" defer></script> -->
     <!-- <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js" defer></script> -->
 
