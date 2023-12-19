05d2c2e0cd0c7fe2f38d3ac6a9786394e58b1ae8
Roy <95271521+Ryotard@users.noreply.github.com>
2023-12-19 22:22:13 +0800

    Update index_custodian.php

--- index_custodian.php
+++ index_custodian.php
@@ -244,9 +244,11 @@ $id = $_SESSION['user_id'];
                                         <?php
                                         if($row['custodian_notif'] == 1){
 
-                                            echo $row['message'] . $row['dateTime'];
+                                            echo 'transfer has been approved by the supplier at' . $row['dateTime'];
                                         }elseif(($row['custodian_notif'] == 0)){
                                             echo 'transfer has been rejected by the supplier at' . $row['dateTime'];
+                                            echo 'Reason: ' . $row['message'];
+
                                         }
                                         ?>
                                     </div>
